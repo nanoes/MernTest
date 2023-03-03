237adb5bc932501e90804f26c68268d0d0aa26be
Matias Flores <110851830+matiasFront@users.noreply.github.com>
2023-03-03 17:18:01 -0300

    second commit

---
+++ models/BlogModel.js
@@ -0,0 +1,13 @@
+//import the connection to DB
+
+import db from "../database/db.js";
+
+//import sequelize
+import { DataTypes } from "sequelize";
+
+const BlogModel = db.define('blogs', {
+    title: { type: DataTypes.STRING },
+    content: { type: DataTypes.STRING },
+});
+
+export default BlogModel;
